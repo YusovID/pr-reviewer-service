af041f649960c46b9579e2bc90d0401700c1aed0
YusovID <yusovilya@gmail.com>
2025-12-16 19:37:06 +0300

    remove golang-ci lint from tools and dependencies

--- tools/tools.go
+++ tools/tools.go
@@ -5,6 +5,5 @@ package tools
 
 import (
 	_ "github.com/golang-migrate/migrate/v4/cmd/migrate"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
 )
